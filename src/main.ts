4dee91d061854ec41bbf707bc9f1c1d1e8f73706
JotaErrE <jruiz967@gmail.com>
2025-01-12 18:34:02 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,12 @@ async function bootstrap() {
     })
   );
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Si necesitas compartir cookies
+  });
 
   app.setGlobalPrefix('api');
 
