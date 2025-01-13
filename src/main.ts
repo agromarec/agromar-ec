9243e82c42b2a327d735b98d951b8beda645b9cb
JotaErrE <jruiz967@gmail.com>
2025-01-12 20:19:20 -0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Si necesitas compartir cookies
   });
 
   app.setGlobalPrefix('api');
