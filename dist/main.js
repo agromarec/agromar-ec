90cf1acd38b3c366b29f2602b40b0976ddebaa39
JotaErrE <jruiz967@gmail.com>
2025-01-12 20:37:16 -0500

    fix: update files

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+const handler = (req, res) => {
+    const d = new Date();
+    res.end(d.toString());
+};
+module.exports = allowCors(handler);
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({
@@ -11,7 +27,7 @@ async function bootstrap() {
     }));
     app.enableCors({
         origin: '*',
-        methods: 'GET,POST,PUT,DELETE,OPTIONS',
+        methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
         allowedHeaders: 'Content-Type, Authorization',
     });
     app.setGlobalPrefix('api');
